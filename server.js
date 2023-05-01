60274884647355653debadb775a633f0a6b9edb0
Arikrishnan01 <krishari11052001@gmail.com>
2023-05-01 13:12:54 -0700

    email tools

---
+++ server.js
@@ -0,0 +1,170 @@
+const express = require('express');
+let nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const creds = require('./credential.json');
+
+let app = express();
+
+ const path = require('path');
+ app.use('/public', express.static(path.join(__dirname, 'public')));
+
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({extended: true}));
+
+ /**nodemailer transpoter */
+let transpoter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: creds.auth.user,
+        pass: creds.auth.pass
+    }
+ })
+
+app.post('/mail', (req, res, next) => {
+    var email = req.body.email
+    var message = req.body.message
+    var subject = req.body.subject
+    var name = req.body.name
+    var company = req.body.company
+
+    const mailOptions = {
+        from: name,
+        to: email,
+        subject: subject,
+        html: `${name} from ${company} <noreplay@${name}.com> <br /> ${message}`
+    }
+
+    transpoter.sendMail(mailOptions, (err, data) => {
+        if(err){
+            res.json({
+                status: err
+            })
+            console.log(err)
+        }
+        else{
+            res.json({
+                status: "success"
+            })
+            console.log("Email Sent"+ data.response)
+        }
+    })
+})
+    transpoter.verify(function(err, success) {
+        if(err){
+            console.log(err)
+        }
+        else{
+            console.log("Server is ready to take email")
+        }
+    })
+
+ const PORT =process.env.PORT || 8000
+
+ app.listen(PORT, ()=> {
+    console.info(`Server started ${PORT}`)
+ });
+
+
+
+
+// let express = require('express');
+// let app = express();
+// let nodemailer = require('nodemailer');
+// const bodyParser = require('body-parser');
+// const creds = require('./credential.json');
+
+// const path = require('path');
+// const router = express.Router()
+
+// // Static folder
+// app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({extended: true}))
+
+// let transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com", 
+//     port: 587,
+//     secure: false,
+//   // service: 'Gmail',
+//   auth: {
+//     user: creds.auth.user,
+//     pass: creds.auth.pass 
+//   },
+// });
+
+// app.post('/mail', (req, res, next) => {
+//     var email = req.body.email
+//     var message = req.body.message
+//     var subject = req.body.subject
+//     var name = req.body.name
+//     var company = req.body.company
+
+    // const mailOptions = {
+    //     from :  name,
+    //     to : email,
+    //     subject: subject,
+    //     html: `${name} from ${company} <noreply@${name}.com> <br /><br /> ${message}`
+    // }
+
+//     transporter.sendMail(mailOptions, (err, data) => {
+//         if(err){
+//             res.json({
+//                 status:"err"
+//             }) 
+//             console.log(err)
+//             }
+//             else {
+//                 res.json({
+//                     status: "success"
+//          })
+//          console.log("Email Sent" + data.response)
+//         }
+//     })
+// })
+
+// transporter.verify(function(error, success) {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log("Server is ready to take our messages!");
+//     }
+//   });
+
+// // serve PORT running here
+// const PORT = process.env.PORT || 8080
+// app.listen(PORT, () => console.info(`server has started on ${PORT}`))
+
+/*** ------------------------------------ */
+
+
+/**server listen with port number */
+// app.listen(PORT,() => {
+//   console.log(`Server started.....`)
+// })
+
+/** ------------------------------ */
+
+
+
+// const express = require('express');
+// const appRoute = require('./routes/route.js')
+// // const bodyParser = require('body-parser');
+
+// const app = express();
+// const PORT = process.env.PORT || 8000;
+
+// app.use(express.json());
+
+// // app.use(bodyParser.json()) // for parsing application/json
+// // app.use(bodyParser.urlencoded({ extended: true }))
+
+// /** routes */
+// app.use('/api', appRoute);
+
+// /** server listen with port */
+// app.listen(PORT, () => {
+//   console.log(`Server listening on port ${PORT}`)
+// });
